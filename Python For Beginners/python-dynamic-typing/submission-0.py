8a6c6cfa9f64622cbccedb434f7859f6fc1b6d14
kevin_simo <77506405+FaceKnowledgeEver@users.noreply.github.com>
2026-07-06 14:24:09 +0200

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.2
+print(type(variable))
+variable = True
+print(type(variable))
+variable = 'python'
+print(type(variable))
+variable = [1.2,2.3]
+print(type(variable))
